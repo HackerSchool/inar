49c21e6fb1e7cf4e03749e8d4e76de378f5a931f
Ricardo Antunes <me@riscadoa.com>
2022-09-28 18:09:11 +0100

    Add state, constants, options and main files. The options file parses command line arguments which will be useful for testing.

--- scripts/main.py
+++ scripts/main.py
@@ -1,64 +1,9 @@
-from core import Parser, Robot, Protocol
-from constants import UPDATE_RATE
-from control import MimicBehaviour
-
-import cv2
-import time
+from options import Parser
 
 def run(options):
     """The main entry point for the controller."""
-
-    robot = Robot()
-    protocol = Protocol(not options["viewer"], not options["receiver"])
-    behaviour = MimicBehaviour()
-
-    # TODO: allow changing the mode of the robot, e.g. using the protocol
-
-    if not options["single"]:
-        # Setup connection.
-        if options["ip"] is not None:
-            protocol.connect(options["ip"], options["port"])
-        else:
-            protocol.listen(options["port"])
-    elif options["receiver"] or options["viewer"]:
-        raise RuntimeError("In single mode state and frames can't be received.")
-
-    if not options["viewer"]:
-        cap = cv2.VideoCapture(0)
-
-    # Then, run the main loop.
-    start = time.time()
-    acc = 0
-    while True:
-        # Calculate the time since the last update.
-        now = time.time()
-        acc += now - start
-        start = now
-
-        # Only update when enough time has passed.
-        if acc >= 1 / UPDATE_RATE:
-            acc -= 1 / UPDATE_RATE
-
-            # If we're not receiving frames, capture one.
-            if not options["viewer"]:
-                success, frame = cap.read()
-                if not success:
-                    raise RuntimeError("Failed to capture frame.")
-                robot.setFrame(frame)
-            
-            # Communicate with the other side if we're not in single mode.
-            if not options["single"]:
-                protocol.update(robot)
-
-            # Update the robot state, if we're not receiving it.
-            if not options["receiver"]:
-                frame = robot.getFrame()
-                robot.setTargetState(behaviour.update(robot))
-                robot.update(1 / UPDATE_RATE)
-
-            # Update servos
-            if options["servos"]:
-                pass # TODO: update the servos.
+    # TODO: init robot
+    # TODO: main loop, update at the configured rate
 
 if __name__ == "__main__":
     parser = Parser()
@@ -67,9 +12,6 @@ if __name__ == "__main__":
     parser.add("servos", "Controls the servos.", default=False)
     parser.add("ip", "The IP address to connect to.")
     parser.add("port", "The port to listen/connect to.", default=10000)
-    parser.add("receiver", "If set, will receive state instead of calculating it.", default=False)
-    parser.add("viewer", "If set, will receive frames instead of capturing them.", default=False)
-    parser.add("single", "If set, won't listen for connections.", default=False)
     options = parser.parse()
     if options["help"]:
         print(parser.help())
