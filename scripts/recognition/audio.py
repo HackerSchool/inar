a054d9443bba12b4edcaf0916ca074feb61cde21
rita_gama <rita.gama@tecnico.ulisboa.pt>
2023-01-04 17:54:09 +0000

    Integrated audio recognition into main structure

--- scripts/recognition/audio.py
+++ scripts/recognition/audio.py
@@ -1,18 +1,60 @@
-from .emotion import Emotion
-from .phrase import Phrase
+# from .emotion import Emotion
+# from .phrase import Phrase
 
-def emotionFromAudio(audio) -> Emotion:
-    """
-    Extracts the emotion from a voice.
-    """
-    emotion = Emotion.NEUTRAL
-    # TODO: implement this
-    return emotion
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import json
 
-def phraseFromAudio(audio) -> Phrase:
-    """
-    Extracts the phrase from a voice.
-    """
-    phrase = Phrase.UNKNOWN
-    # TODO: implement this
-    return phrase
+def captureAudio():
+    model = Model(r"./vosk-model-small-en-us-0.15") 
+    recognizer = KaldiRecognizer(model,  16000) #TODO: search the frequency and the class
+
+    # Recognize from microphone
+    capture = pyaudio.PyAudio()
+    stream = capture.open(format= pyaudio.paInt16, channels=1, rate= 16000, input=True, frames_per_buffer=8192)
+    stream.start_stream()
+
+    return Audio(stream, recognizer)
+
+
+class Audio():
+    
+    def __init__(self, stream, recognizer) -> None:
+        self.stream = stream
+        self.recognizer = recognizer
+
+    # def emotionFromAudio(self) -> Emotion:
+    #     """
+    #     Extracts the emotion from a voice.
+    #     """
+    #     emotion = Emotion.NEUTRAL
+    #     # TODO: implement this
+    #     return emotion
+
+    def phraseFromAudio(self):
+        """
+        Extracts the phrase from a voice.
+        """
+        text = ""
+
+        while True:
+            data = self.stream.read(4096)
+            if self.recognizer.AcceptWaveform(data):
+                #https://github.com/alphacep/vosk-api/blob/master/python/test/transcribe_scp.py
+                res = json.loads(self.recognizer.Result()) 
+                #text += " " + res["text"]
+                text = res["text"]
+                print(text)
+
+        #phrase = Phrase.UNKNOWN
+        # TODO: stop at end of x time or at bye/stop instruction
+
+        #return phrase
+
+
+# def main():
+#     audio = captureAudio()
+
+#     audio.phraseFromAudio()
+
+# main()
--- scripts/recognition/speech-recognition.py
+++ scripts/recognition/speech-recognition.py
@@ -1,5 +1,6 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
+import json
 
 model = Model(r"./vosk-model-small-en-us-0.15") 
 recognizer = KaldiRecognizer(model,  16000) #TODO: search the frequency and the class
@@ -10,7 +11,13 @@ capture = pyaudio.PyAudio()
 stream = capture.open(format= pyaudio.paInt16, channels=1, rate= 16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
+text = ""
+
 while True:
     data = stream.read(4096)
     if recognizer.AcceptWaveform(data):
-        print(recognizer.Result())
+        #https://github.com/alphacep/vosk-api/blob/master/python/test/transcribe_scp.py
+        res = json.loads(recognizer.Result()) 
+        text += " " + res["text"]
+        #text = res["text"]
+        print(text)
